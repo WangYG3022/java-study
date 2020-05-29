7c6ba1e61bdab042ca372740379635f0e30fa891
Y.G. Wang <935203723@qq.com>
2020-05-29 23:26:37 +0800

    Redis入门Demo

---
+++ redis/src/main/java/com/wangyaguang/study/redis/config/RedissonConfig.java
@@ -0,0 +1,29 @@
+package com.wangyaguang.study.redis.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: 单机版redisson
+ * @author: WANG Y.G.
+ * @time: 2019/10/30 21:52
+ */
+@Configuration
+public class RedissonConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private String port;
+    @Value("${spring.redis.database}")
+    private int database;
+    
+    @Bean
+    public Redisson redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setDatabase(database);
+        return (Redisson) Redisson.create(config);
+    }
+}
